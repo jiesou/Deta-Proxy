4d2beac0680e0af65a2b1f2fb1360ec4a5971e88
jiesou <jiechushou@gmail.com>
2023-04-27 00:18:23 +0800

    fine-tuning code

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Deta-Proxy
 Simple HTTP Network Proxy in Deta
+
+Example:
+
+```
+https://xxxx-deta.app/https://example.com
+```
+
+Support for protocols that are not explicitly specified, example:
+
+```
+https://xxxx-deta.app/example.com
+```
+equals to
+```
+https://xxxx-deta.app/http://example.com
+```
+
+Supports forwarding headers, body (readable stream) and so on.
--- index.js
+++ index.js
@@ -5,37 +5,31 @@ var app = express();
 
 
 function throwRequest(res, error) {
-    res.status(500)
-        .end('<!DOCTYPE html>PROXY ERROR: ' + error + '<br>Please refer the doc: https://loliis.top/proxy.html#通用');
+    res.status(500).json({
+      code: 500,
+      message: `PROXY ERROR: ${error}`
+    });
 }
 
 app.all('*', async (req, res) => {
-    //去掉路径斜线
+    // remove first slash
     let url = req.path.substr(1);
-    if (!url) {
-        throwRequest(res, 'nullValue');
-    }
-    //添加 http:// 统一 :// 的斜线数量
+    if (!url) throwRequest(res, 'nullValue');
+
+    // add http:// and limit :// number of slashes
     url = url.replace(/^(http(s?):\/+)?/, 'http$2://');
-    console.log(url)
     const req_headers = req.headers;
-    //请求的 headers 去掉 referer
-    if (req_headers['referer']) {
-        delete req_headers['referer'];
-    }
-    //请求 headers host 里的 proxy.gfwin.icu 改成代理的链接域名
+    // remove referer in headers
+    if (req_headers['referer']) delete req_headers['referer'];
+    // turn headers.host into the host of target
     req_headers['host'] = new URL(url).host;
     
-    try{
+    try {
         const response = await fetch(url, {
           method: req.method,
           headers: req_headers,
           body: req.body
-        })
-        if (!response.ok) {
-          throwRequest(res, "request error: " + response.status);
-        }
-        //添加响应的 headers
+        });
         res.writeHead(response.status, response.headers);
         response.body.pipe(res);
     } catch (e) {
